ccb0016d2224e72c2db4d8cb553ad7938cbeb75d
zebasheikh <zebasheikh18@gmail.com>
2019-05-14 11:42:04 +0530

    first commit

---
+++ src/main/java/com/stackroute/stackroute/domain/Movie.java
@@ -0,0 +1,30 @@
+package com.stackroute.stackroute.domain;
+
+public class Movie {
+    Actor actor;
+
+
+
+    public Movie() {
+    }
+
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "actor=" + actor +
+                '}';
+    }
+}
