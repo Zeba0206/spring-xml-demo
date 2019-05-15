cace7de52bb4bdb455bf9b5161b58dbe0d354fe0
Zeba0206 <49705130+Zeba0206@users.noreply.github.com>
2019-05-15 10:17:12 +0530

    Update Main.java
    update main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,24 +1,5 @@
 /*
-Task 2:
-From the master branch of spring-xml-demo repo create a constructor-injection
-branch.
-Add constructor to the Actor class to initialize with name and gender, and age
-Create three beans of type Actor in the bean definition file.
-Use constructor-based injection in the bean definition file (beans.xml) to inject property values in
-each of the three beans via name, index, and type respectively.
-For the Movie bean, use constructor based object injection in the bean definition file
-(beans.xml) to inject an Actor bean.
-In the Main class, look up Movie bean using ApplicationContext and print out Author
-information.
-Use the same ApplicationContext to again look up the same Movie bean.
-Print out the equality result of the two Movie beans.
-System.out.println(beanA==beanB);
-Change the scope of the Movie bean in beans.xml to prototype and run the application again.
-Note the output.
-Replace id of the Movie bean with name having two values, like this:
-<bean name=”MovieA, MovieB” ……..>
-Update the code in Main to get the Movie bean by its two different name.
-Push the code to constructor-injection branch. 
+From the constructor-injection branch of spring-xml-demo repo create a autowire-xml branch. For the Movie bean, delete the constructor based object injection in the bean definition file (beans.xml) that injects an Actor bean. Use autowire byName in the Movie bean to inject an Actor bean. Run the application. Create another Movie bean and try autowire byType. Run the application and note the exception thrown. Fix the Movie bean by removing autowire byType and using constructor injection instead. Push the code to autowire-xml branch. 
  */
 package com.stackroute;
 
@@ -30,16 +11,14 @@ public class Main {
     public static void main(String[] args) {
         // Task-1 using Application context
         ApplicationContext bean=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie1 =(Movie) bean.getBean("MovieA");
-        Movie movie2=(Movie)bean.getBean("movie2");
+        
+        Movie movie=(Movie)bean.getBean("movie1");
+        
+        System.out.println(movie.toString());
 
-        Movie movie3=(Movie)bean.getBean("MovieB");
+        Movie movi31=(Movie)applicationContext3.getBean("movie2");
+        
+        System.out.println(movie3.toString());
 
-
-        System.out.println(movie1.toString());
-        System.out.println(movie2.toString());
-
-
-        System.out.println(movie3==movie2);
     }
 }
